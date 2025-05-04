0a50bcb7669983409237ed23c79c3cdd2ee2660b
JAMES22308 <michaeljamessoria@gmail.com>
2025-05-05 08:54:38 +1200

    added_folder function (improved)

--- main.py
+++ main.py
@@ -10,39 +10,43 @@ def get_list(options):
 def display(items):
     sort = sorted(items)
     for i in sort:
-        print(i)
-
+        if i.endswith('.txt'):
+            print(f'{i.replace('.txt', '')}')
+        else:
+            print(i)
+        
 
 
 def add_folder(directory):
     
     if not os.path.exists(directory):
         os.mkdir(directory)
-    
-    ask = input('create a folder: ')
-    char = any(char in "()*&^%$#@!~`,.;':?}{[]" for char in ask )
-    error = ask[0] == '/' or ask[-1] == '/'
-    fullpath = os.path.join(directory, ask)
-    if not os.path.exists(fullpath):
-        try:
-            if not error and not char:
-                os.mkdir(fullpath)
-                print('folder has been created')
-            else:
-                print('try again')
-        except FileNotFoundError:
-            print('cannot make a subfolder')
-        
-    else:
-        print('already existing')
-  
+    try:
+        ask = input('create a folder: ')
+        char = any(char in "()*&^%$#@!~`,.;':?}{[]" for char in ask )
+        error = ask[0] == '/' or ask[-1] == '/'
+        fullpath = os.path.join(directory, ask)
+        if not os.path.exists(fullpath):
+            try:
+                if not error and not char:
+                    os.mkdir(fullpath)
+                    print('folder has been created')
+                else:
+                    print('try again')
+            except FileNotFoundError:
+                print('cannot make a subfolder')
+            
+        else:
+            print('already existing')
+    except IndexError:
+        print("index error")
 
 
 
 def find_folder(directory):
     if not os.path.exists(directory):
         output = "no directory at the moment"
-        return output
+        return print(output)
 
     
     folders = os.listdir(directory)
@@ -51,22 +55,20 @@ def find_folder(directory):
     for index, folder in enumerate(sort, start=1):
         print(f"{index}. {folder}")
     
-    ask = input('enter the folder name: ')
+    ask = input('enter the folder name: ').strip()
     if not ask:
-        return 'try again'
+        print('try again')
     else:
         fullpath = os.path.join(directory, ask)
         if os.path.exists(fullpath):
             files = os.listdir(fullpath)
             if files:
-                for file in files:
-                    print(file)
+                display(files)
             else:
-                return 'no content'
+                print('no content')
         else:
-            return 'file does not exist'
-    
-
+            print('file does not exist')
+        
     
 def create_file_txt(prompt):
     ask = input(prompt).strip()
@@ -81,29 +83,39 @@ def create_file(directory):
         display(dir)
         ask = input('select a folder: ')
         if ask in dir:
-            ask2 = input('press 1 new file | press 2 add content | press enter to exit: ')
-            if ask2 == '1':
-                ask3 = create_file_txt('file name: ')
-                fullpath = os.path.join(directory, ask, ask3)
-                with open(fullpath, 'w')as file: 
-                    file.write('')
-                    print('new file has been created')
-            elif ask2 == '2':
-                full = os.path.join(directory, ask)
-                full_path = os.listdir(full)
-                display(full_path)
-                ask4 = input('select a file: ')
-                joinpath = os.path.join(full, ask4)
-                if os.path.exists(joinpath):
-                    ask5 = input('note: ')
-                    print('note list: ')
-                    with open(joinpath, 'a')as file:
-                        file.write(f"{ask5}\n") 
-                    with open(joinpath, 'r')as file:
-                        con = file.read()
-                    print(con)
-                            
+            while True:
+                ask2 = input('press 1 new file | press 2 add content | press enter to exit: ')
+                if ask2 == '1':
+                    ask3 = create_file_txt('file name: ')
+                    fullpath = os.path.join(directory, ask, ask3)
+                    with open(fullpath, 'w')as file: 
+                        file.write('')
+                        print('new file has been created')
+                elif ask2 == '2':
+                    full = os.path.join(directory, ask)
+                    full_path = os.listdir(full)
+                    
 
+                    display(full_path)
+                    ask4 = input('select a file: ')
+                    joinpath = os.path.join(full, ask4 + ".txt")
+                    if os.path.exists(joinpath):
+                        while True:
+                            ask5 = input('add note: ')
+                            if ask5 == "":
+                                return False
+                            else:
+                                print('note list: ')
+                                with open(joinpath, 'a')as file:
+                                    file.write(f"{ask5}\n") 
+                                with open(joinpath, 'r')as file:
+                                    con = file.read()
+                                print(con)
+                elif ask2 == '':
+                    return False
+                
+                else:
+                    print('wrong key')
 
     except FileNotFoundError:
         print('file not found error')
@@ -130,7 +142,7 @@ def main():
         if ask == '1':
             add_folder(directory)
         elif ask == '2':
-            print(find_folder(directory))
+            find_folder(directory)
         elif ask == '3':
             create_file(directory)
         
